e1a15bb9066f60c265d6f98b7304aac86966650e
franciscaabsr <francisca.absr@gmail.com>
2018-12-20 16:08:18 +0000

    input preparation and script to run project

--- main.m
+++ main.m
@@ -1,18 +1,36 @@
+% Prepare parameters and access to functions
 clear all
-close all 
+close all
 
-d = dir ('*.jpg'); %lists the files in directory .jpg (all the images)
-dd = dir('*.mat'); %lists the files in directory .mat (all the depth images)
-load ('.\CalibData\cameraparametersAsus.mat');
-imgs = [];
+load ('./CalibData/cameraparametersAsus.mat');
+addpath('natsortfiles/');
 
-for i = 1: length (d)
-    im = imread(d(i).name);
-    imgs = cat(3,imgs,im); 
-    load(dd(i).name);
-    imgsd(:,:,i) = double(depth_array)/1000; 
-    [r,c] = ind2sub(size(imgsd(:,:,i)),find(imgsd(:,:,i)));
-    im_vec = reshape(imgsd(:,:,i),[480*640,1]);
-    xyz_depth = get_xyz_asus(im_vec, [480, 640], [r, c], cam_params.Kdepth, 1, 0);
-    rgbd = get_rgbd(xyz_depth, im, cam_params.R, cam_params.T, cam_params.Krgb);
+% Insert data in array of structures
+
+r1 = dir ('./rgb_image1_*'); %lists the files in directory .png (all the images)
+d1 = dir('./depth1_*'); %lists the files in directory .mat (all the depth images)
+r2 =  dir ('./rgb_image2_*'); %lists the files in directory .png (all the images)
+d2 = dir('./depth2_*'); %lists the files in directory .mat (all the depth images)
+
+% Sort in alphanumeric order
+r1_sorted = natsortfiles({r1.name})';
+d1_sorted = natsortfiles({d1.name})';
+r2_sorted = natsortfiles({r2.name})';
+d2_sorted = natsortfiles({d2.name})';
+
+% Add everyhting to the imgseq arrays for both cameras
+for i=1:length(r1)
+    imgseq1(i).rgb= char(r1_sorted(i));
+    imgseq2(i).rgb= char(r2_sorted(i));
+    imgseq1(i).depth= char(d1_sorted(i));
+    imgseq2(i).depth= char(d2_sorted(i));
+end
+
+prompt = 'Insert 1 if you wish to run project for 1 camera or 2 if you wish to run for 2 cameras: ';
+choice = input(prompt);
+
+if choice == 1
+    objects = track3D_part1(imgseq1, cam_params);
+elseif choice == 2
+    [objects, cam2toW] = track3D_part2(imgseq1, imgseq2, cam_params);
 end
